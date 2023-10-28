10a1df4ed3509fa2ac6dba15f2530aae1a9e19ce
Shuster Oleksandr <godzilasanya@gmail.com>
2023-10-28 16:48:40 +0300

    task-2 Done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+//створення рядка розмітки
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+  )
+  .join("");
+
+//додованяя елементів в DOM
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("beforeend", markup);
+
+//ініціалізація
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
